016a5d75f51cd6e453661d37b979643712526869
Joel Wiebe <jwiebe17@yahoo.com>
2018-07-20 15:07:48 -0500

    first commit

---
+++ StudentMangement/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using StudentMangement.Models;
+
+namespace StudentMangement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public StudentsEntities db = new StudentsEntities();
+
+        public ActionResult Index()
+        {
+            List<Degree> DegreeList = db.Degrees.ToList();
+            ViewBag.ListOfDegree = new SelectList(DegreeList, "DegreeID", "DegreeName");
+            return View();
+        }
+
+        public JsonResult GetStudentList()
+        {
+            List<StudentViewModel> StuList = db.Students.Where(x => x.IsDeleted == false).Select(x => new StudentViewModel
+            {
+                StudentId = x.StudentId,
+                Name = x.Name,
+                PhoneNumber = x.PhoneNumber,
+                Address = x.Address,
+                DegreeName = x.Degree.DegreeName,
+                
+            }).ToList();
+
+            return Json(StuList, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult GetStudentById(int StudentId)
+        {
+            Student model = db.Students.Where(x => x.StudentId == StudentId).SingleOrDefault();
+            string value = string.Empty;
+            value = JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return Json(value, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveDataInDatabase(StudentViewModel model)
+        {
+            var result = false;
+            try
+            {
+                if(model.StudentId > 0)
+                {
+                    Student Stu = db.Students.SingleOrDefault(x => x.IsDeleted == false && x.StudentId == model.StudentId);
+                    Stu.Name = model.Name;
+                    Stu.Address = model.Address;
+                    Stu.PhoneNumber = model.PhoneNumber;
+                    Stu.DegreeID = model.DegreeID;
+                    db.SaveChanges();
+                    result = true;
+                    
+                }
+                else
+                {
+                    Student Stu = new Student();
+                    Stu.Name = model.Name;
+                    Stu.Address = model.Address;
+                    Stu.PhoneNumber = model.PhoneNumber;
+                    Stu.DegreeID = model.DegreeID;
+                    Stu.IsDeleted = false;
+                    db.Students.Add(Stu);
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult DeleteStudentRecord(int StudentId)
+        {
+            bool result = false;
+            Student Stu = db.Students.SingleOrDefault(x => x.IsDeleted == false && x.StudentId == StudentId);
+            if (Stu != null)
+            {
+                Stu.IsDeleted = true;
+                db.SaveChanges();
+                result = true; 
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+       
+    }
+}
---
+++ StudentMangement/Models/StudentViewModel.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentMangement.Models
+{
+    public class StudentViewModel
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public Nullable<int> DegreeID { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+
+        public string DegreeName { get; set; }
+
+        //Instance Variables
+        public IBuyCreditsBehavior buyCreditsBehavior;
+        public IRegisterClassBehavior registerClassBehavior;
+        public IRetrieveTranscript retrieveTranscriptBehavior;
+
+        //Delegate to Behavior class
+        public void BuyCafeteriaCredits()
+        {
+            buyCreditsBehavior.buyCredits();
+        }
+        public void RegisterForClass()
+        {
+            registerClassBehavior.regClass();
+        }
+        public void RetrieveTranscript()
+        {
+            retrieveTranscriptBehavior.retTranscript();
+        }
+    }
+
+    //Buy Credits interface
+    public interface IBuyCreditsBehavior
+    {
+        void buyCredits();
+    }
+
+    //Buy Credits Implementation Classes
+    public class DocBuyCredits : IBuyCreditsBehavior
+    {
+
+        public void buyCredits()
+        {
+            Console.WriteLine("Bought Doctorate Credits");
+        }
+
+    }
+    public class UnderGradBuyCredits : IBuyCreditsBehavior
+    {
+
+        public void buyCredits()
+        {
+            Console.WriteLine("Bought UndegGraduate Credits");
+        }
+    }
+    public class GradBuyCredits : IBuyCreditsBehavior
+    {
+
+        public void buyCredits()
+        {
+            Console.WriteLine("Bought Graduate Credits");
+        }
+    }
+
+    //Register Class Interface
+    public interface IRegisterClassBehavior
+    {
+        void regClass();
+    }
+
+    //Register Class Implementation classes
+    public class DocRegisterForClass : IRegisterClassBehavior
+    {
+        public void regClass()
+        {
+            Console.WriteLine("Registered Doctorate Class");
+        }
+    }
+    public class UnderGradRegisterForClass : IRegisterClassBehavior
+    {
+        public void regClass()
+        {
+            Console.WriteLine("Registered Undergraduate Class");
+        }
+    }
+    public class GradRegisterForClass : IRegisterClassBehavior
+    {
+        public void regClass()
+        {
+            Console.WriteLine("Registered Graduate Class");
+        }
+    }
+
+    //Retrieve Transcript Interface
+    public interface IRetrieveTranscript
+    {
+        void retTranscript();
+    }
+
+    //Retrieve Transcript Implementation Classes
+    public class DocRetrieveTranscript : IRetrieveTranscript
+    {
+        public void retTranscript()
+        {
+            Console.WriteLine("Retrieved Doctorate Transcript");
+        }
+    }
+    public class UnderGradRetrieveTranscript : IRetrieveTranscript
+    {
+        public void retTranscript()
+        {
+            Console.WriteLine("Retrieved Undergraduate Transcript");
+        }
+    }
+    public class GradRetrieveTranscript : IRetrieveTranscript
+    {
+        public void retTranscript()
+        {
+            Console.WriteLine("Retrieved Graduate Transcript");
+        }
+    }
+
+    public class Doctorate : StudentViewModel
+    {
+        public void DoctorateBuyCafeteriaCredits()
+        {
+            buyCreditsBehavior = new DocBuyCredits();
+        }
+        public void DoctorateRegisterForClass()
+        {
+            registerClassBehavior = new DocRegisterForClass();
+        }
+        public void DoctorateRetrieveTranscript()
+        {
+            retrieveTranscriptBehavior = new DocRetrieveTranscript();
+        }
+    }
+
+    public class Graduate : StudentViewModel
+    {
+        public void GraduateBuyCafeteriaCredits()
+        {
+            buyCreditsBehavior = new GradBuyCredits();
+        }
+        public void GraduateRegisterForClass()
+        {
+            registerClassBehavior = new GradRegisterForClass();
+        }
+        public void GraduateRetrieveTranscript()
+        {
+            retrieveTranscriptBehavior = new GradRetrieveTranscript();
+        }
+    }
+
+    public class UnderGraduate : StudentViewModel
+    {
+        public void UnderGradBuyCafeteriaCredits()
+        {
+            buyCreditsBehavior = new UnderGradBuyCredits();
+        }
+        public void UnderGradRegisterForClass()
+        {
+            registerClassBehavior = new UnderGradRegisterForClass();
+        }
+        public void UnderGradRetrieveTranscript()
+        {
+            retrieveTranscriptBehavior = new UnderGradRetrieveTranscript();
+        }
+    }
+}
